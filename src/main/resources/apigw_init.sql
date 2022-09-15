79eaf5efe5e652da576ca5efac5e834f9ad8c4a1
Seongyun Kim <seongyun.kim@keti.re.kr>
2022-09-15 21:56:50 +0900

    DDL 수정

--- src/main/resources/apigw_init.sql
+++ src/main/resources/apigw_init.sql
@@ -9,18 +9,9 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-CREATE DATABASE apigw WITH TEMPLATE = template0 ENCODING = 'UTF8'
+CREATE DATABASE apigw WITH TEMPLATE = template0 ENCODING = 'UTF8';
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+\c apigw;
 
 CREATE SCHEMA apigw;
 
