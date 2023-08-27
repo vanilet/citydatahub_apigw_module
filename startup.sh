4cf2ba2d5ede2f8323939288e58ccbc92f80e7e8
Cheol-Min Kim <cheolminkim@vanilet.pe.kr>
2023-08-27 17:14:30 +0900

    - API-GW 버전 1.1.0으로 변경 - 데이터코어 DML 업데이트 - startup script가 원할히 동작할 수 있도록 변경

--- startup.sh
+++ startup.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+mkdir -p logs
+
 java    \
         -server \
-        -jar apigw-1.0.0.jar > /dev/null 2>&1 &
+        -jar apigw-1.1.0.jar > /dev/null 2>&1 &
+
+sleep 2
+
 tail -f ./logs/apigw.log
